422face5a7cd7759158f790027e0e9a82858aca9
Shobhit150 <vshobhit150@gmail.com>
2026-01-27 00:06:45 +0530

    temp

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import asyncio
+import httpx
+from services.scrap import scrap_page
 app = FastAPI()
+
+client = httpx.AsyncClient(timeout=10)
 
 @app.websocket("/ws")
 async def server(ws: WebSocket):
-    count = 0
+    
     await ws.accept()
 
     try :
         while True:
-            await ws.send_text(str(count))
-            count += 1
-            await asyncio.sleep(1)
+            res = await scrap_page("https://www.bseindia.com/markets/PublicIssues/OFSIssuse_new.aspx?expandable=0", client)
+            await asyncio.sleep(5)
+
     except WebSocketDisconnect:
         print("client disconnected")
 
--- services/scrap.py
+++ services/scrap.py
@@ -9,6 +9,6 @@ async def scrap_page(url: str) -> dict:
 
     soup = BeautifulSoup(resp.text, "html.parser")
 
-    print(soup.div.text)
+    print(soup)
 
-asyncio.run(scrap_page("https://www.bseindia.com/markets/PublicIssues/OFSIssuse_new.aspx?expandable=0"))
+asyncio.run(scrap_page("https://www.bseindia.com/markets/PublicIssues/BSEbidDetails_ofs.aspx?flag=NR&Scripcode=544282"))
